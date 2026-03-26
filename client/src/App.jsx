2dae3c1dbe9747ebd4df58fda9e295cfd6eaee66
bwithtea <bwithtea@gmail.com>
2026-03-26 16:06:43 -0400

    add map search UI with image display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,36 @@
-
+import { useState } from "react"
+import axios from "axios"
 
 function App() {
+  const [search, setSearch] = useState('')
+  const [maps, setMaps] = useState([])
+
+  async function handleSearch(){
+    const response = await axios.get(`http://localhost:3001/api/maps/search?name=${search}`)
+    setMaps(response.data)
+    console.log(response.data)
+  }
+
+
   return (
     <div>
       <h1>BrawlEdge</h1>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Search a map..."
+      
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      {maps.map(map => (
+        <div key={map.id}>
+          {map.name}
+          <img src={map.imageUrl} alt={map.name}/>
+        </div>
+      ))}
+
     </div>
   )
 }
